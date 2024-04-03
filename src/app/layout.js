c181d594308ff779d374f79fd870186d752f3ebc
OYELEKE EMMANUEL OLUSOLA <94241498+emmanuelolusola@users.noreply.github.com>
2024-04-03 04:04:46 +0100

    homepage seo: in progress

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,40 +35,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        {/* Open Graph tags */}
-        <meta property="og:type" content={metadata.openGraph.type} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content={metadata.openGraph.image.url} />
-        <meta
-          property="og:image:width"
-          content={metadata.openGraph.image.width}
-        />
-        <meta
-          property="og:image:height"
-          content={metadata.openGraph.image.height}
-        />
-        <meta property="og:site_name" content={metadata.openGraph.site_name} />
-        {/* Twitter Card tags */}
-        <meta name="twitter:card" content={metadata.twitter.type} />
-        <meta name="twitter:url" content={metadata.twitter.url} />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content={metadata.twitter.image.url} />
-        <meta
-          name="twitter:image:width"
-          content={metadata.twitter.image.width}
-        />
-        <meta
-          name="twitter:image:height"
-          content={metadata.twitter.image.height}
-        />
-        <meta name="twitter:site" content={metadata.twitter.site_name} />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
