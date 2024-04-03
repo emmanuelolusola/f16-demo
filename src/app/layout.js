8bf44a9e8474fd00138d9d6a5fd0570c8d4404ec
OYELEKE EMMANUEL OLUSOLA <94241498+emmanuelolusola@users.noreply.github.com>
2024-04-03 04:05:20 +0100

    homepage seo: in progress

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,36 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Friends of 16",
-  description:
-    "16/16 is a serene space for intimate experiences designed to attract and serve creatives",
-  openGraph: {
-    type: "website",
-    url: "https://f16-next-demo.vercel.app",
-    image: {
-      url: "https://f16-next-demo.vercel.app/SEOImage.png",
-      width: 1200,
-      height: 628,
-      alt: "logo",
-    },
-    site_name: "Friends of 16",
-  },
-  twitter: {
-    type: "card",
-    url: "https://f16-next-demo.vercel.app",
-    image: {
-      url: "https://f16-next-demo.vercel.app/SEOImage.png",
-      width: 1200,
-      height: 628,
-      alt: "logo",
-    },
-    site_name: "Friends of 16",
-  },
-};
 
 export default function RootLayout({ children }) {
   return (
