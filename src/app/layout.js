2d01d60a1b3f73a96957f2bddeef2ad82878e41b
OYELEKE EMMANUEL OLUSOLA <94241498+emmanuelolusola@users.noreply.github.com>
2024-04-03 03:24:43 +0100

    homepage seo: in progress

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export const metadata = {
     type: "website",
     url: "https://f16-next-demo.vercel.app",
     image: {
-      url: "https://th.bing.com/th/id/R.10065b85bf94fe2c30af58ff87f241a3?rik=HK2mxCiayZ9wFA&pid=ImgRaw&r=0",
+      url: "https://f16-next-demo.vercel.app/SEOImage.png",
       width: 1200,
       height: 628,
       alt: "logo",
@@ -25,21 +25,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:type" content={metadata.openGraph.type} />
-        <meta property="og:url" content={metadata.openGraph.url} />
-        <meta property="og:image" content={metadata.openGraph.image.url} />
         <meta
-          property="og:image:width"
-          content={metadata.openGraph.image.width}
+          property="og:image"
+          content="https://f16-next-demo.vercel.app/SEOImage.png"
         />
-        <meta
-          property="og:image:height"
-          content={metadata.openGraph.image.height}
-        />
-        <meta property="og:image:alt" content={metadata.openGraph.image.alt} />
-        <meta property="og:site_name" content={metadata.openGraph.site_name} />
+        <meta property="og:image:type" content="website" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
