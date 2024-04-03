a3778929d2809d03123eb15bb7525630e54b6afa
OYELEKE EMMANUEL OLUSOLA <94241498+emmanuelolusola@users.noreply.github.com>
2024-04-03 03:32:25 +0100

    homepage seo: in progress

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,19 +19,55 @@ export const metadata = {
     },
     site_name: "Friends of 16",
   },
+  twitter: {
+    type: "card",
+    url: "https://f16-next-demo.vercel.app",
+    image: {
+      url: "https://f16-next-demo.vercel.app/SEOImage.png",
+      width: 1200,
+      height: 628,
+      alt: "logo",
+    },
+    site_name: "Friends of 16",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        {/* Open Graph tags */}
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.openGraph.image.url} />
         <meta
-          property="og:image"
-          content="https://f16-next-demo.vercel.app/SEOImage.png"
+          property="og:image:width"
+          content={metadata.openGraph.image.width}
         />
-        <meta property="og:image:type" content="website" />
-        <meta property="og:image:width" content="<generated>" />
-        <meta property="og:image:height" content="<generated>" />
+        <meta
+          property="og:image:height"
+          content={metadata.openGraph.image.height}
+        />
+        <meta property="og:site_name" content={metadata.openGraph.site_name} />
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content={metadata.twitter.type} />
+        <meta name="twitter:url" content={metadata.twitter.url} />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content={metadata.twitter.image.url} />
+        <meta
+          name="twitter:image:width"
+          content={metadata.twitter.image.width}
+        />
+        <meta
+          name="twitter:image:height"
+          content={metadata.twitter.image.height}
+        />
+        <meta name="twitter:site" content={metadata.twitter.site_name} />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
