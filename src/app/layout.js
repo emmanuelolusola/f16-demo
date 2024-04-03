d407c78257759772ac7cc2e12cf0904e2f6ba143
OYELEKE EMMANUEL OLUSOLA <94241498+emmanuelolusola@users.noreply.github.com>
2024-04-03 04:37:54 +0100

    homepage seo: in progress

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,13 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Friends of 16",
   description: "lorem ipsum",
+  image: <Image src="/SEOImage.png" alt="" width={1200} height={628} />,
 };
 
 export default function RootLayout({ children }) {
